e1fe9880c04dde3a29d8f8218614d3d71760e538
Timo Betcke <timo.betcke@gmail.com>
2019-08-26 13:31:51 +0100

    Updates to barycentric test.

--- bempp/api/space/test/test_barycentric_rwg.py
+++ bempp/api/space/test/test_barycentric_rwg.py
@@ -7,21 +7,23 @@ import pytest
 
 pytestmark = pytest.mark.usefixtures("default_parameters", "helpers")
 
-def test_rwg_barycentric_on_reference_triangle():
-    """Test B-RWG space on unit triangle."""
+def test_rwg_barycentric(helpers, precision):
+    """Test B-RWG space."""
     import bempp.api
+    import math
 
-    grid = bempp.api.shapes.reference_triangle()
+    grid = bempp.api.shapes.regular_sphere(3)
     space = bempp.api.function_space(grid, "RWG", 0, include_boundary_dofs=True)
     space_bary = space.barycentric_representation
-    assert space_bary.global_dof_count == 3
 
     # Define a function on this space
 
-    coeffs = _np.array([0, 0, 1], dtype='float64')
+    rand = _np.random.RandomState(0)
+    coeffs = rand.randn(space.global_dof_count)
 
     fun = bempp.api.GridFunction(space, coefficients=coeffs)
     fun_bary = bempp.api.GridFunction(space_bary, coefficients=coeffs)
-    fun_bary.l2_norm()
 
+    assert math.isclose(fun.l2_norm(), fun_bary.l2_norm(),
+            rel_tol=helpers.default_tolerance(precision))
 
