56c2c705d4bff249e6ab5607d08c0b464fd44b00
Timo Betcke <timo.betcke@gmail.com>
2020-04-17 01:11:59 +0100

    Working on ExaFmm integration.

--- bempp/api/fmm/helpers.py
+++ bempp/api/fmm/helpers.py
@@ -5,63 +5,140 @@ M_INV_4PI = 1.0 / (4 * _np.pi)
 
 
 @_numba.jit(
-    nopython=True, parallel=False, error_model="numpy", fastmath=True, boundscheck=False
+nopython=True, parallel=False, error_model="numpy", fastmath=True, boundscheck=False
 )
-def laplace_kernel(target_point, source_point, kernel_parameters, dtype):
+def laplace_kernel(target_points, source_points, kernel_parameters, dtype, result_type):
     """Evaluate the laplace kernel."""
 
-    diff = target_point - source_point
+    ntargets = target_points.shape[1]
+    nsources = source_points.shape[1]
+    m_inv_4pi = dtype.type(M_INV_4PI)
 
-    dist = diff[0] * diff[0] + diff[1] * diff[1] + diff[2] * diff[2]
-    dist = _np.sqrt(dist)
+    interactions = _np.empty(4 * ntargets * nsources, dtype=result_type)
+    diff = _np.zeros((3, nsources), dtype=dtype)
+    for target_point_index in range(ntargets):
+        dist = _np.zeros(nsources, dtype=dtype)
+        for i in range(3):
+            for j in range(nsources):
+                diff[i, j] = target_points[i, target_point_index] - source_points[i, j]
+                dist[j] += diff[i, j] * diff[i, j]
+        for j in range(nsources):
+            dist[j] = _np.sqrt(dist[j])
+        for j in range(nsources):
+            interactions[target_point_index * 4 * nsources + 4 * j] = (
+                m_inv_4pi / dist[j]
+            )
+        for i in range(3):
+            for j in range(nsources):
+                interactions[target_point_index * 4 * nsources + 4 * j + 1 + i] = (
+                    -diff[i, j] * m_inv_4pi / (dist[j] * dist[j] * dist[j])
+                )
+        # Now fix zero distance case
+        for j in range(nsources):
+            if dist[j] == 0:
+                for i in range(4):
+                    interactions[target_point_index * 4 * nsources + 4 * j + i] = 0
 
-    result = _np.empty(4, dtype=dtype)
-    green = dtype.type(M_INV_4PI) / dist
+    return interactions
 
-    result[0] = green
-    result[1:4] = -diff * green / (dist * dist)
 
-    return result
+def get_local_interaction_matrix(
+    grid, local_points, kernel_function, kernel_parameters, precision, is_complex
+):
+
+    from bempp.api.utils.helpers import get_type
+    from scipy.sparse import coo_matrix
+
+    npoints = local_points.shape[1]
+
+    dtype = _np.dtype(get_type(precision).real)
+    if is_complex:
+        result_type = _np.dtype(get_type(precision).complex)
+    else:
+        result_type = dtype
+
+    data, iind, jind = get_local_interaction_matrix_impl(
+        grid.data(precision),
+        local_points.astype(dtype),
+        kernel_function,
+        _np.array(kernel_parameters, dtype=dtype),
+        dtype,
+        result_type,
+    )
+
+    rows = 4 * npoints * grid.number_of_elements
+    cols = npoints * grid.number_of_elements
+
+    return coo_matrix((data, (iind, jind)), shape=(rows, cols)).tocsr()
 
 
 @_numba.jit(
-    nopython=True, parallel=True, error_model="numpy", fastmath=True, boundscheck=False
+nopython=True, parallel=True, error_model="numpy", fastmath=True, boundscheck=False
 )
-def evaluate_interactions_on_singular_elements(
-    grid_data, charges, local_points, kernel_function, kernel_parameters, dtype
+def get_local_interaction_matrix_impl(
+    grid_data, local_points, kernel_function, kernel_parameters, dtype, result_type
 ):
-    """Evaluate charges on the singular elements."""
-
+    """Get the local interaction matrix on the grid."""
     nelements = grid_data.elements.shape[1]
     npoints = local_points.shape[1]
     neighbor_indices = grid_data.element_neighbor_indices
     neighbor_indexptr = grid_data.element_neighbor_indexptr
 
-    result = _np.zeros((4, nelements), dtype=dtype)
+    data = _np.zeros(4 * npoints * npoints * len(neighbor_indices), dtype=result_type)
+    iind = _np.zeros(4 * npoints * npoints * len(neighbor_indices), dtype=_np.uint32)
+    jind = _np.zeros(4 * npoints * npoints * len(neighbor_indices), dtype=_np.uint32)
 
-    for element_index in _numba.prange(nelements):
-        element_points = grid_data.local2global(element_index, local_points)
-        nneighbors = neighbor_indexptr[1 + element_index] - neighbor_indexptr[element_index]
-        for n_index in range(nneighbors):
-            neighbor = neighbor_indices[neighbor_indexptr[element_index] + n_index]
-            neighbor_points = grid_data.local2global(neighbor, local_points)
-            for element_point_index in range(npoints):
-                for neighbor_point_index in range(npoints):
-                    if neighbor == element_index and element_point_index == neighbor_point_index:
-                        continue
-                    result[:, element_index] += (
-                        kernel_function(
-                            element_points[:, element_point_index],
-                            neighbor_points[:, neighbor_point_index],
-                            kernel_parameters,
-                            dtype
+    global_points = _np.zeros((nelements, 3, npoints), dtype=dtype)
+
+    for element_index in range(nelements):
+        global_points[element_index, :, :] = grid_data.local2global(
+            element_index, local_points
+        )
+
+    for target_element in _numba.prange(nelements):
+        target_points = global_points[target_element]
+        nneighbors = (
+            neighbor_indexptr[1 + target_element] - neighbor_indexptr[target_element]
+        )
+        source_elements = neighbor_indices[
+            neighbor_indexptr[target_element] : neighbor_indexptr[1 + target_element]
+        ]
+        local_source_points = _np.empty((3, npoints * nneighbors), dtype=dtype)
+        for source_element_index in range(nneighbors):
+            source_element = neighbor_indices[
+                neighbor_indexptr[target_element] + source_element_index
+            ]
+            local_source_points[
+                :, npoints * source_element_index : npoints * (1 + source_element_index)
+            ] = global_points[source_element, :, :]
+        local_target_points = global_points[target_element, :, :]
+        interactions = kernel_function(
+            local_target_points,
+            local_source_points,
+            kernel_parameters,
+            dtype,
+            result_type,
+        )
+
+        local_count = 4 * npoints * npoints * neighbor_indexptr[target_element]
+        for source_element_index in range(nneighbors):
+            source_element = neighbor_indices[
+                neighbor_indexptr[target_element] + source_element_index
+            ]
+            for target_point_index in range(npoints):
+                for source_point_index in range(npoints):
+                    for i in range(4):
+                        data[local_count] = interactions[4 * target_point_index * npoints + 4 * source_point_index + i]
+                        iind[local_count] = (
+                            4 * (npoints * target_element + target_point_index) + i
                         )
-                        * charges[neighbor * npoints + neighbor_point_index]
-                    )
-    return result.sum(axis=1)
+                        jind[local_count] = npoints * source_element + source_point_index
+                        local_count += 1
+
+    return data, iind, jind
 
 
-def map_space_to_points(space, local_points, weights, mode, return_transpose=False):
+def map_space_to_points(space, local_points, weights, return_transpose=False):
     """Return mapper from grid coeffs to point evaluations."""
     import bempp.api
     from scipy.sparse import coo_matrix
@@ -73,7 +150,7 @@ def map_space_to_points(space, local_points, weights, mode, return_transpose=Fal
     number_of_vertices = number_of_local_points * grid.number_of_elements
 
     data, global_indices, vertex_indices = map_space_to_points_impl(
-        grid.data,
+        grid.data('double'),
         space.localised_space.local2global,
         space.localised_space.local_multipliers,
         space.localised_space.normal_multipliers,
@@ -155,3 +232,37 @@ def map_space_to_points_impl(
         )
 
     return (data, global_indices, vertex_indices)
+
+
+@_numba.njit
+def grid_to_points(grid_data, local_points):
+    """
+    Map a grid to an array of points.
+
+    Returns a (N, 3) point array that stores the global vertices
+    associated with the local points in each triangle.
+    Points are stored in consecutive order for each element 
+    in the support_elements list. Hence, the returned array is of the form
+    [ v_1^1, v_2^1, ..., v_M^1, v_1^2, v_2^2, ...], where
+    v_i^j is the ith point in the jth element in
+    the support_elements list.
+
+    Parameters
+    ----------
+    grid_data : GridData
+        A Bempp GridData object.
+    local_points : np.ndarray
+        (2, M) array of local coordinates.
+    """
+    number_of_elements = grid_data.elements.shape[1]
+    number_of_points = local_points.shape[1]
+
+    points = _np.empty((number_of_points * number_of_elements, 3), dtype=_np.float64)
+
+    for elem in range(number_of_elements):
+        points[number_of_points * elem : number_of_points * (1 + elem), :] = (
+            _np.expand_dims(grid_data.vertices[:, grid_data.elements[0, elem]], 1)
+            + grid_data.jacobians[elem].dot(local_points)
+        ).T
+    return points
+
