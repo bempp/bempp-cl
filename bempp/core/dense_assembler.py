db846a7f307b8abcf23aee6a1c72b471be85d970
Timo Betcke <timo.betcke@gmail.com>
2020-04-27 19:10:35 +0100

    CL Dense Assembler workiing.

--- bempp/core/dense_assembler.py
+++ bempp/core/dense_assembler.py
@@ -30,13 +30,13 @@ class DenseAssembler(_assembler.AssemblerBase):
             )
 
         # (
-            # numba_assembly_function_regular,
-            # numba_kernel_function_regular,
+        # numba_assembly_function_regular,
+        # numba_kernel_function_regular,
         # ) = select_numba_kernels(operator_descriptor, mode="regular")
 
         # (
-            # numba_assembly_function_singular,
-            # numba_kernel_function_singular,
+        # numba_assembly_function_singular,
+        # numba_kernel_function_singular,
         # ) = select_numba_kernels(operator_descriptor, mode="singular")
 
         mat = assemble_dense(
@@ -52,14 +52,12 @@ class DenseAssembler(_assembler.AssemblerBase):
 
         return DenseDiscreteBoundaryOperator(mat)
 
+
 def assemble_dense(
-        domain,
-        dual_to_range,
-        parameters,
-        operator_descriptor,
-        device_interface,
-        ):
+    domain, dual_to_range, parameters, operator_descriptor, device_interface
+):
     """Assembles the operator and returns a dense matrix."""
+    import bempp.api
     from bempp.api.utils.helpers import get_type
     from bempp.core.dispatcher import dense_assembler_dispatcher
     from bempp.core.singular_assembler import assemble_singular_part
@@ -76,13 +74,17 @@ def assemble_dense(
 
     result = _np.zeros((rows, cols), dtype=result_type)
 
-    dense_assembler_dispatcher(
-        device_interface,
-        operator_descriptor,
-        domain,
-        dual_to_range,
-        parameters,
-        result)
+    with bempp.api.Timer(
+        message=f"Regular assembler:{operator_descriptor.identifier}:{device_interface}"
+    ):
+        dense_assembler_dispatcher(
+            device_interface,
+            operator_descriptor,
+            domain,
+            dual_to_range,
+            parameters,
+            result,
+        )
 
     grids_identical = domain.grid == dual_to_range.grid
 
@@ -113,115 +115,116 @@ def assemble_dense(
 
     return result
 
+
 # @_timeit
 # def assemble_dense(
-    # domain,
-    # dual_to_range,
-    # parameters,
-    # operator_descriptor,
-    # numba_assembly_function_regular,
-    # numba_kernel_function_regular,
-    # numba_assembly_function_singular,
-    # numba_kernel_function_singular,
+# domain,
+# dual_to_range,
+# parameters,
+# operator_descriptor,
+# numba_assembly_function_regular,
+# numba_kernel_function_regular,
+# numba_assembly_function_singular,
+# numba_kernel_function_singular,
 # ):
-    # """
-    # Really assemble the operator.
-    # Assembles the complete operator (near-field and far-field)
-    # Returns a dense matrix.
-    # """
-    # from bempp.api.integration.triangle_gauss import rule as regular_rule
-    # from bempp.api import log
-    # from bempp.core.numba.singular_assembler import assemble_singular_part
-    # from bempp.api.utils.helpers import get_type
-    # import bempp.api
+# """
+# Really assemble the operator.
+# Assembles the complete operator (near-field and far-field)
+# Returns a dense matrix.
+# """
+# from bempp.api.integration.triangle_gauss import rule as regular_rule
+# from bempp.api import log
+# from bempp.core.numba.singular_assembler import assemble_singular_part
+# from bempp.api.utils.helpers import get_type
+# import bempp.api
 
-    # order = parameters.quadrature.regular
-    # quad_points, quad_weights = regular_rule(order)
+# order = parameters.quadrature.regular
+# quad_points, quad_weights = regular_rule(order)
 
-    # test_indices, test_color_indexptr = dual_to_range.get_elements_by_color()
-    # trial_indices, trial_color_indexptr = domain.get_elements_by_color()
-    # number_of_test_colors = len(test_color_indexptr) - 1
-    # number_of_trial_colors = len(trial_color_indexptr) - 1
+# test_indices, test_color_indexptr = dual_to_range.get_elements_by_color()
+# trial_indices, trial_color_indexptr = domain.get_elements_by_color()
+# number_of_test_colors = len(test_color_indexptr) - 1
+# number_of_trial_colors = len(trial_color_indexptr) - 1
 
-    # rows = dual_to_range.global_dof_count
-    # cols = domain.global_dof_count
+# rows = dual_to_range.global_dof_count
+# cols = domain.global_dof_count
 
-    # nshape_test = dual_to_range.number_of_shape_functions
-    # nshape_trial = domain.number_of_shape_functions
+# nshape_test = dual_to_range.number_of_shape_functions
+# nshape_trial = domain.number_of_shape_functions
 
-    # precision = operator_descriptor.precision
+# precision = operator_descriptor.precision
 
-    # data_type = get_type(precision).real
-    # if operator_descriptor.is_complex:
-        # result_type = get_type(precision).complex
-    # else:
-        # result_type = get_type(precision).real
+# data_type = get_type(precision).real
+# if operator_descriptor.is_complex:
+# result_type = get_type(precision).complex
+# else:
+# result_type = get_type(precision).real
 
-    # result = _np.zeros((rows, cols), dtype=result_type)
+# result = _np.zeros((rows, cols), dtype=result_type)
 
-    # grids_identical = domain.grid == dual_to_range.grid
+# grids_identical = domain.grid == dual_to_range.grid
 
-    # dense_assembler_dispatcher(
-            # device_interface,
-            # operator_descriptor,
-            # domain,
-            # dual_to_range,
+# dense_assembler_dispatcher(
+# device_interface,
+# operator_descriptor,
+# domain,
+# dual_to_range,
 
 
-    # with bempp.api.Timer(message="Start Numba dense assembler."):
-        # for test_color_index in range(number_of_test_colors):
-            # numba_assembly_function_regular(
-                # dual_to_range.grid.data(precision),
-                # domain.grid.data(precision),
-                # nshape_test,
-                # nshape_trial,
-                # test_indices[
-                    # test_color_indexptr[test_color_index] : test_color_indexptr[
-                        # 1 + test_color_index
-                    # ]
-                # ],
-                # trial_indices,
-                # dual_to_range.local_multipliers.astype(data_type),
-                # domain.local_multipliers.astype(data_type),
-                # dual_to_range.local2global,
-                # domain.local2global,
-                # dual_to_range.normal_multipliers,
-                # domain.normal_multipliers,
-                # quad_points.astype(data_type),
-                # quad_weights.astype(data_type),
-                # numba_kernel_function_regular,
-                # _np.array(operator_descriptor.options, dtype=data_type),
-                # grids_identical,
-                # dual_to_range.shapeset.evaluate,
-                # domain.shapeset.evaluate,
-                # result,
-            # )
+# with bempp.api.Timer(message="Start Numba dense assembler."):
+# for test_color_index in range(number_of_test_colors):
+# numba_assembly_function_regular(
+# dual_to_range.grid.data(precision),
+# domain.grid.data(precision),
+# nshape_test,
+# nshape_trial,
+# test_indices[
+# test_color_indexptr[test_color_index] : test_color_indexptr[
+# 1 + test_color_index
+# ]
+# ],
+# trial_indices,
+# dual_to_range.local_multipliers.astype(data_type),
+# domain.local_multipliers.astype(data_type),
+# dual_to_range.local2global,
+# domain.local2global,
+# dual_to_range.normal_multipliers,
+# domain.normal_multipliers,
+# quad_points.astype(data_type),
+# quad_weights.astype(data_type),
+# numba_kernel_function_regular,
+# _np.array(operator_descriptor.options, dtype=data_type),
+# grids_identical,
+# dual_to_range.shapeset.evaluate,
+# domain.shapeset.evaluate,
+# result,
+# )
 
-    # if grids_identical:
-        # # Need to treat singular contribution
+# if grids_identical:
+# # Need to treat singular contribution
 
-        # trial_local2global = domain.local2global.ravel()
-        # test_local2global = dual_to_range.local2global.ravel()
-        # trial_multipliers = domain.local_multipliers.ravel()
-        # test_multipliers = dual_to_range.local_multipliers.ravel()
+# trial_local2global = domain.local2global.ravel()
+# test_local2global = dual_to_range.local2global.ravel()
+# trial_multipliers = domain.local_multipliers.ravel()
+# test_multipliers = dual_to_range.local_multipliers.ravel()
 
-        # singular_rows, singular_cols, singular_values = assemble_singular_part(
-            # domain.localised_space,
-            # dual_to_range.localised_space,
-            # parameters,
-            # operator_descriptor,
-            # numba_assembly_function_singular,
-            # numba_kernel_function_singular,
-        # )
+# singular_rows, singular_cols, singular_values = assemble_singular_part(
+# domain.localised_space,
+# dual_to_range.localised_space,
+# parameters,
+# operator_descriptor,
+# numba_assembly_function_singular,
+# numba_kernel_function_singular,
+# )
 
-        # rows = test_local2global[singular_rows]
-        # cols = trial_local2global[singular_cols]
-        # values = (
-            # singular_values
-            # * trial_multipliers[singular_cols]
-            # * test_multipliers[singular_rows]
-        # )
+# rows = test_local2global[singular_rows]
+# cols = trial_local2global[singular_cols]
+# values = (
+# singular_values
+# * trial_multipliers[singular_cols]
+# * test_multipliers[singular_rows]
+# )
 
-        # _np.add.at(result, (rows, cols), values)
+# _np.add.at(result, (rows, cols), values)
 
-    # return result
+# return result
--- bempp/core/opencl_assemblers.py
+++ bempp/core/opencl_assemblers.py
@@ -136,7 +136,11 @@ def dense_assembler(
     from bempp.api.integration.triangle_gauss import rule
     from bempp.api.utils.helpers import get_type
     from bempp.core.opencl_kernels import get_kernel_from_operator_descriptor
-    from bempp.core.opencl_kernels import default_context, default_device
+    from bempp.core.opencl_kernels import (
+        default_context,
+        default_device,
+        get_vector_width,
+    )
 
     mf = _cl.mem_flags
     ctx = default_context()
@@ -144,21 +148,25 @@ def dense_assembler(
 
     precision = operator_descriptor.precision
     dtype = get_type(precision).real
+    kernel_options = operator_descriptor.options
 
     quad_points, quad_weights = rule(parameters.quadrature.regular)
 
     test_indices, test_color_indexptr = dual_to_range.get_elements_by_color()
     trial_indices, trial_color_indexptr = domain.get_elements_by_color()
 
+    number_of_test_colors = len(test_color_indexptr) - 1
+    number_of_trial_colors = len(trial_color_indexptr) - 1
+
     options = {
-            "NUMBER_OF_QUAD_POINTS": len(quad_weights),
-            "TEST": dual_to_range.shapeset.identifier,
-            "TRIAL": domain.shapeset.identifier,
-            "TRIAL_NUMBER_OF_ELEMENTS": domain.number_of_support_elements,
-            "TEST_NUMBER_OF_ELEMENTS": dual_to_range.number_of_support_elements,
-            "NUMBER_OF_TEST_SHAPE_FUNCTIONS": dual_to_range.number_of_shape_functions,
-            "NUMBER_OF_TRIAL_SHAPE_FUNCTIONS": domain.number_of_shape_functions,
-            }
+        "NUMBER_OF_QUAD_POINTS": len(quad_weights),
+        "TEST": dual_to_range.shapeset.identifier,
+        "TRIAL": domain.shapeset.identifier,
+        "TRIAL_NUMBER_OF_ELEMENTS": domain.number_of_support_elements,
+        "TEST_NUMBER_OF_ELEMENTS": dual_to_range.number_of_support_elements,
+        "NUMBER_OF_TEST_SHAPE_FUNCTIONS": dual_to_range.number_of_shape_functions,
+        "NUMBER_OF_TRIAL_SHAPE_FUNCTIONS": domain.number_of_shape_functions,
+    }
 
     if operator_descriptor.is_complex:
         options["COMPLEX_KERNEL"] = None
@@ -170,8 +178,12 @@ def dense_assembler(
         operator_descriptor, options, "regular", force_novec=True
     )
 
-    test_indices_buffer = _cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=test_indices)
-    trial_indices_buffer = _cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=trial_indices)
+    test_indices_buffer = _cl.Buffer(
+        ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=test_indices
+    )
+    trial_indices_buffer = _cl.Buffer(
+        ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=trial_indices
+    )
 
     test_normals_buffer = _cl.Buffer(
         ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=dual_to_range.normal_multipliers
@@ -180,14 +192,134 @@ def dense_assembler(
         ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=domain.normal_multipliers
     )
     test_grid_buffer = _cl.Buffer(
-        ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=dual_to_range.grid.as_array.astype(dtype)
+        ctx,
+        mf.READ_ONLY | mf.COPY_HOST_PTR,
+        hostbuf=dual_to_range.grid.as_array.astype(dtype),
     )
     trial_grid_buffer = _cl.Buffer(
         ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=domain.grid.as_array.astype(dtype)
+    )
 
     test_elements_buffer = _cl.Buffer(
-        ctx, mf.READ_ONLY | mf.COPY_HOST_TR, hostbuf=dual_to_range.grid.elements.ravel(order='F')
+        ctx,
+        mf.READ_ONLY | mf.COPY_HOST_PTR,
+        hostbuf=dual_to_range.grid.elements.ravel(order="F"),
+    )
 
     trial_elements_buffer = _cl.Buffer(
-        ctx, mf.READ_ONLY | mf.COPY_HOST_TR, hostbuf=domain.grid.elements.ravel(order='F')
+        ctx,
+        mf.READ_ONLY | mf.COPY_HOST_PTR,
+        hostbuf=domain.grid.elements.ravel(order="F"),
+    )
 
+    test_local2global_buffer = _cl.Buffer(
+        ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=dual_to_range.local2global
+    )
+
+    trial_local2global_buffer = _cl.Buffer(
+        ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=domain.local2global
+    )
+
+    test_multipliers_buffer = _cl.Buffer(
+        ctx,
+        mf.READ_ONLY | mf.COPY_HOST_PTR,
+        hostbuf=dual_to_range.local_multipliers.astype(dtype),
+    )
+
+    trial_multipliers_buffer = _cl.Buffer(
+        ctx,
+        mf.READ_ONLY | mf.COPY_HOST_PTR,
+        hostbuf=domain.local_multipliers.astype(dtype),
+    )
+
+    quad_points_buffer = _cl.Buffer(
+        ctx,
+        mf.READ_ONLY | mf.COPY_HOST_PTR,
+        hostbuf=quad_points.ravel(order="F").astype(dtype),
+    )
+
+    quad_weights_buffer = _cl.Buffer(
+        ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=quad_weights.astype(dtype)
+    )
+
+    result_buffer = _cl.Buffer(ctx, mf.READ_WRITE, size=result.nbytes)
+
+    if not kernel_options:
+        kernel_options = [0.0]
+
+    kernel_options_array = _np.array(kernel_options, dtype=dtype)
+
+    kernel_options_buffer = _cl.Buffer(
+        ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=kernel_options_array
+    )
+
+    vector_width = get_vector_width(precision)
+
+    def kernel_runner(
+        queue,
+        test_offset,
+        trial_offset,
+        test_number_of_indices,
+        trial_number_of_indices,
+    ):
+        """Actually run the kernel for a given range."""
+        remainder_size = trial_number_of_indices % vector_width
+        main_size = trial_number_of_indices - remainder_size
+
+        buffers = [
+            test_indices_buffer,
+            trial_indices_buffer,
+            test_normals_buffer,
+            trial_normals_buffer,
+            test_grid_buffer,
+            trial_grid_buffer,
+            test_elements_buffer,
+            trial_elements_buffer,
+            test_local2global_buffer,
+            trial_local2global_buffer,
+            test_multipliers_buffer,
+            trial_multipliers_buffer,
+            quad_points_buffer,
+            quad_weights_buffer,
+            result_buffer,
+            kernel_options_buffer,
+            _np.int32(dual_to_range.global_dof_count),
+            _np.int32(domain.global_dof_count),
+            _np.uint8(domain.grid != dual_to_range.grid),
+        ]
+
+        if main_size > 0:
+            main_kernel(
+                queue,
+                (test_number_of_indices, main_size // vector_width),
+                (1, 1),
+                *buffers,
+                global_offset=(test_offset, trial_offset),
+            )
+
+        if remainder_size > 0:
+            remainder_kernel(
+                queue,
+                (test_number_of_indices, remainder_size),
+                (1, 1),
+                *buffers,
+                global_offset=(test_offset, trial_offset + main_size),
+            )
+
+    with _cl.CommandQueue(ctx, device=device) as queue:
+        _cl.enqueue_fill_buffer(queue, result_buffer, _np.uint8(0), 0, result.nbytes)
+        for test_index in range(number_of_test_colors):
+            test_offset = test_color_indexptr[test_index]
+            n_test_indices = (
+                test_color_indexptr[1 + test_index] - test_color_indexptr[test_index]
+            )
+            for trial_index in range(number_of_trial_colors):
+                n_trial_indices = (
+                    trial_color_indexptr[1 + trial_index]
+                    - trial_color_indexptr[trial_index]
+                )
+                trial_offset = trial_color_indexptr[trial_index]
+                kernel_runner(
+                    queue, test_offset, trial_offset, n_test_indices, n_trial_indices
+                )
+        _cl.enqueue_copy(queue, result, result_buffer)
