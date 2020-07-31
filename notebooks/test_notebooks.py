5dcc2bace6c9df991d58b15cdf5b436fde076030
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-07-31 07:27:17 +0100

    remove kernel (was needed when using anaconda)

--- notebooks/test_notebooks.py
+++ notebooks/test_notebooks.py
@@ -13,17 +13,12 @@ for dir in ["laplace", "helmholtz", "maxwell"]:
         if i.endswith(".ipynb") and i not in large_problems:
             notebooks.append((os.path.join("notebooks", dir), i))
 
-kernel = "python3"
-if "KERNEL_NAME" in os.environ:
-    kernel = os.environ["KERNEL_NAME"]
 
-
-@pytest.mark.parametrize('kernel_name', [kernel])
 @pytest.mark.parametrize(('path', 'notebook'), notebooks)
 def test_notebook(path, notebook, kernel_name):
     with open(os.path.join(path, notebook)) as f:
         nb = nbformat.read(f, as_version=4)
 
-    ep = ExecutePreprocessor(timeout=600, kernel_name=kernel_name)
+    ep = ExecutePreprocessor(timeout=600)
 
     ep.preprocess(nb, {'metadata': {'path': path}})
