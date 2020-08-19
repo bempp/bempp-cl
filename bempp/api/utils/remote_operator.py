94b3d72374b3a76c5de2fcc84a6c1054569879e4
Timo Betcke <timo.betcke@gmail.com>
2020-08-19 15:39:46 +0100

    First implementation of remote blocked op. working.

--- bempp/api/utils/remote_operator.py
+++ bempp/api/utils/remote_operator.py
@@ -27,7 +27,6 @@ class Message:
         self.nelements = nelements
 
 
-
 class RemoteManager:
     """Manage remote worker execution."""
 
@@ -37,7 +36,6 @@ class RemoteManager:
         self._tags = {}
         self._tags_counter = 0
         self._rank_counter = 1
-
 
     def register(self, op):
         """
@@ -55,7 +53,6 @@ class RemoteManager:
         self._rank_counter = (1 + self._rank_counter) % MPI_SIZE
         if self._rank_counter == 0:
             self._rank_counter = 1
-
 
     def execute_worker(self):
         """Only execute on workers."""
@@ -113,23 +110,26 @@ class RemoteManager:
         else:
             return message.status, message.operator_tag, None
 
-    def submit_computation(self, tag, x):
-        """Submit computation to operator (only execute on rank 0)."""
+    def submit_computation(self, op, x):
+        """Submit computation to operator."""
 
+        tag = self._tags[op]
         rank = self._op_location[tag]
 
         self.send_data("DATA", rank, tag, x)
 
-    def receive_result(self, tag):
+    def receive_result(self, op):
         """Receive result from a specific operator."""
 
+        tag = self._tags[op]
         rank = self._op_location[tag]
         _, _, data = self.receive_data(rank)
         return data
 
-    def assemble(self, tag):
+    def assemble(self, op):
         """Assemble a given remote operator."""
 
+        tag = self._tags[op]
         rank = self._op_location[tag]
         self.send_data("ASSEMBLE", rank, tag)
 
@@ -162,42 +162,80 @@ class RemoteManager:
         for worker in range(1, MPI_SIZE):
             self.send_data("SHUTDOWN", worker)
 
+    def _group_by_rank(self, tags):
+        """
+        Group tags by for parallel computation.
+
+        Given a list of tags (with possible repetitions).
+        Return a list of list of positions in the tags list
+        that is sorted so that each sublist does not have repeating
+        ranks. This allows to distribute the computations effectively
+        on the ranks.
+        """
+
+        output = []
+
+        tags_with_indices = {index: tag for index, tag in enumerate(tags)}
+        
+        while len(tags_with_indices) > 0:
+            current_indices = []
+            current_ranks = []
+            for index, tag in tags_with_indices.items():
+                rank = self._op_location[tag]
+                if rank not in current_ranks:
+                    current_indices.append(index)
+                    current_ranks.append(rank)
+            for index in current_indices:
+                del tags_with_indices[index]
+            output.append(current_indices)
+
+        return output
+
+
+    def assemble_parallel(self, ops):
+        """Assemble a list of operators in parallel."""
+
+        tags = [self._tags[op] for op in ops]
+        grouped_indices = self._group_by_rank(tags)
+        
+        for index_list in grouped_indices:
+            for index in index_list:
+                self.assemble(ops[index])
+            self.barrier()
+
 
     def compute_parallel(self, tasks):
         """
-        Schedule parallel computations.
+        Compute in parallel
 
         Parameters
         ----------
-        tasks : dict
-            A dictionary {tag1: data1, tag2: data2, ...}
-            of tags and associated vectors.
+        tasks : list
+            A list [(op1, data1), (op2, data2), ...]
+            of operators and associated vectors.
 
-        Returns a dictionary {tag1: result1, tag2: result2, ...}
+        Returns a list [result1, result2, ...]
         of the results of the computation.
+
+        Operators are allowed to appear repeatedly in the list
+        and are scheduled correctly.
 
         """
 
-        items = list(tasks.items())
-        ranks = {item: self._op_locations[item[0]] for item in items}
-        results = {}
+        tags = [self._tags[op] for op, _ in tasks]
 
-        while items:
-            current_tasks = []
-            rank_set = set()
-            for item in items:
-                if ranks[item] not in rank_set:
-                    task_order.append(item)
-            for task in current_tasks:
-                self.submit_computation(*task)
-            for task in current_tasks:
-                results[task[0]] = self.receive_result(task[0])
-            for task in current_tasks:
-                items.remove(task)
+        results = len(tasks) * [None]
+        grouped_indices = self._group_by_rank(tags)
+        
+        for index_list in grouped_indices:
+            for index in index_list:
+                op, data = tasks[index]
+                self.submit_computation(op, data)
+            for index in index_list:
+                op, data = tasks[index]
+                results[index] = self.receive_result(op)
 
-            
-
-            
+        return results
 
 
     @property
@@ -213,7 +251,6 @@ def get_remote_manager():
     if _REMOTE_MANAGER is None:
         _REMOTE_MANAGER = RemoteManager()
     return _REMOTE_MANAGER
-
 
 
 class RemoteBlockedOperator(_BlockedOperator):
@@ -302,7 +339,14 @@ class RemoteBlockedDiscreteOperator(_DiscreteOperatorBase):
         is_complex = False
         for i in range(rows):
             for j in range(cols):
-                self._manager.assemble(self._tags[i, j])
+                if self._tags[i, j] == -1: continue
+                self._manager.assemble(self._operators[i, j])
+
+        self._manager.barrier()
+
+        for i in range(rows):
+            for j in range(cols):
+                if self._tags[i, j] == -1: continue
                 dtype = self._manager.get_operator_dtype(self._tags[i, j])
                 if dtype in ["complex128", "complex64"]:
                     is_complex = True
@@ -339,6 +383,8 @@ class RemoteBlockedDiscreteOperator(_DiscreteOperatorBase):
         row_dim = 0
         res = _np.zeros(self.shape[0], dtype=combined_type(self.dtype, x.dtype))
 
+        compute_tasks = []
+
         # Submit the computations
         for i in range(self._ndims[0]):
             col_dim = 0
@@ -347,20 +393,24 @@ class RemoteBlockedDiscreteOperator(_DiscreteOperatorBase):
                 if self._tags[i, j] != -1:
                     # If self._tags[i, j] == -1 the operator is None
                     local_x = x[col_dim : col_dim + self._cols[j]]
-                    self._manager.submit_computation(self._tags[i, j], local_x)
+                    compute_tasks.append((self._operators[i, j], local_x))
                 col_dim += self._cols[j]
             row_dim += self._rows[i]
 
+        # Execute computations
+
+        compute_results = self._manager.compute_parallel(compute_tasks)
+
         # Get results back
         row_dim = 0
+        task_count = 0
         for i in range(self._ndims[0]):
             col_dim = 0
             for j in range(self._ndims[1]):
                 if self._tags[i, j] == -1:
                     continue
-                res[row_dim : row_dim + self._rows[i]] += self._manager.receive_result(
-                    self._tags[i, j]
-                )
+                res[row_dim : row_dim + self._rows[i]] += compute_results[task_count]
+                task_count += 1
             row_dim += self._rows[i]
 
         if ndims == 2:
