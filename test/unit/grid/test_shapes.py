9522468222606639fe6408b89d2dc5813a2cf9be
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-01-28 08:27:05 +0000

    add test that shapes can be created

---
+++ test/unit/grid/test_shapes.py
@@ -0,0 +1,20 @@
+import pytest
+import numpy as np
+import bempp.api
+
+
+@pytest.mark.parametrize("gridname, args, kwargs", [
+    ("almond", [], {}),
+    ("cylinders", [], {}),
+    ("reentrant_cube", [], {}),
+    ("screen", [np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]])], {}),
+    ("cube", [], {}),
+    ("ellipsoid", [], {}),
+    ("reference_triangle", [], {}),
+    ("cuboid", [], {}),
+    ("multitrace_cube", [], {}),
+    ("multitrace_sphere", [], {}),
+    ("regular_sphere", [2], {}),
+    ("sphere", [], {})])
+def test_shape(gridname, args, kwargs):
+    getattr(bempp.api.shapes, gridname)(*args, **kwargs)
