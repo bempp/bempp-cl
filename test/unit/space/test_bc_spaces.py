31095078a1c593372e34cb7bf8e61d0687b760cf
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-07-15 09:34:19 +0100

    black

--- test/unit/space/test_bc_spaces.py
+++ test/unit/space/test_bc_spaces.py
@@ -15,11 +15,15 @@ def test_segments(space_type):
 def test_swapped_normals(space_type):
     """Test creation of BC space on multitrace cube with swapped normals."""
     grid = bempp.api.shapes.multitrace_cube(h=1)
-    bempp.api.function_space(grid, space_type, 0, segments=[6, 7, 8, 9, 10, 11], swapped_normals=[6])
+    bempp.api.function_space(
+        grid, space_type, 0, segments=[6, 7, 8, 9, 10, 11], swapped_normals=[6]
+    )
 
 
 @pytest.mark.parametrize("space_type", ["BC", "RBC"])
 def test_segments_6_7(space_type):
     """Test creation of BC space on multitrace cube."""
     grid = bempp.api.shapes.multitrace_cube(h=1)
-    bempp.api.function_space(grid, space_type, 0, segments=[6, 7, 8, 9, 10, 11], swapped_normals=[6])
+    bempp.api.function_space(
+        grid, space_type, 0, segments=[6, 7, 8, 9, 10, 11], swapped_normals=[6]
+    )
