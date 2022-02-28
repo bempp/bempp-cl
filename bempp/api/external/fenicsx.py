dfc80b73858dac635c5eb86e15c1dae69a19b916
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-02-28 08:34:12 +0000

    didn't mean to delete that

--- bempp/api/external/fenicsx.py
+++ bempp/api/external/fenicsx.py
@@ -134,5 +134,8 @@ class FenicsOperator(object):
                 i._ufl_function_space.dofmap.index_map.size_global
                 for i in self._fenics_weak_form.arguments()
             )
+            self._sparse_mat = csr_matrix(
+                (mat.data, mat.indices, mat.indptr), shape=shape
+            )
 
         return SparseDiscreteBoundaryOperator(self._sparse_mat)
