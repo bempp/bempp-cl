386d7a844b0ea1de7dc34919d0df1a56c0206143
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-02-28 08:26:11 +0000

    update fenicsx interface

--- bempp/api/external/fenicsx.py
+++ bempp/api/external/fenicsx.py
@@ -129,6 +129,13 @@ class FenicsOperator(object):
 
         if self._sparse_mat is None:
             mat = assemble_matrix(form(self._fenics_weak_form))
-            self._sparse_mat = csr_matrix((mat.data, mat.indices, mat.indptr), shape=mat.size)
+            mat.finalize()
+            shape = (
+                self._fenics_weak_form.arguments()[0]._ufl_function_space.dofmap.index_map.size_global,
+                self._fenics_weak_form.arguments()[1]._ufl_function_space.dofmap.index_map.size_global,
+            )
+            self._sparse_mat = csr_matrix(
+                (mat.data, mat.indices, mat.indptr), shape=shape
+            )
 
         return SparseDiscreteBoundaryOperator(self._sparse_mat)
--- test/unit/external/test_fenicsx.py
+++ test/unit/external/test_fenicsx.py
@@ -4,7 +4,7 @@ import pytest
 import numpy as np
 from mpi4py import MPI
 import bempp.api
-from bempp.api.external.fenicsx import fenics_to_bempp_trace_data
+from bempp.api.external.fenicsx import fenics_to_bempp_trace_data, FenicsOperator
 
 
 def test_p1_trace(has_dolfinx):
@@ -47,3 +47,30 @@ def test_p1_trace(has_dolfinx):
         fenics_cell = compute_closest_entity(tree, midpoint_tree, fenics_mesh, mid)[0]
         fenics_val = fenics_fun.eval([mid.T], [fenics_cell])
         assert np.isclose(bempp_val[0, 0], fenics_val[0])
+
+
+def test_fenics_operator(has_dolfinx):
+    """Test that a FEniCS operator can be assembled."""
+    try:
+        import ufl
+    except ImportError:
+        if has_dolfinx:
+            raise ImportError("UFL is not installed")
+        pytest.skip("UFL must be installed to run this test")
+    try:
+        from dolfinx.fem import FunctionSpace, Function
+        from dolfinx.mesh import create_unit_cube
+    except ImportError:
+        if has_dolfinx:
+            raise ImportError("DOLFINx is not installed")
+        pytest.skip("DOLFINx must be installed to run this test")
+
+    mesh = create_unit_cube(MPI.COMM_WORLD, 2, 2, 2)
+    space = FunctionSpace(mesh, ("CG", 1))
+
+    u = ufl.TrialFunction(space)
+    v = ufl.TestFunction(space)
+
+    op = FenicsOperator(ufl.inner(u, v) * ufl.dx)
+    weak_op = op.weak_form()
+    weak_op * np.ones(space.dofmap.index_map.size_global)
