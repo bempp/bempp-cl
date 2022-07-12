ba02fd3112593878ab7f99ae57467309db675f1f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-07-12 18:28:33 +0100

    remove install requires

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setup(
     license="MIT",
     include_package_data=True,
     python_requires=">=3.8",
-    install_requires=["numpy", "scipy", "numba>=0.55.2", "meshio"],
-    extras_require={
-        "optional": ["plotly", "pyopencl"],
-        "lint": ["pydocstyle", "flake8", "pytest", "pytest-xdist"],
-    }
+    # install_requires=["numpy", "scipy", "numba>=0.55.2", "meshio"],
+    # extras_require={
+    #    "optional": ["plotly", "pyopencl"],
+    #    "lint": ["pydocstyle", "flake8", "pytest", "pytest-xdist"],
+    # }
 )
