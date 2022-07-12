5f35fcbb5a0dc961be41bbaa491afbfd82d07594
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-07-12 16:56:20 +0100

    fix docker image

--- setup.py
+++ setup.py
@@ -18,4 +18,9 @@ setup(
     license="MIT",
     include_package_data=True,
     python_requires=">=3.8",
+    install_requires=["numpy", "scipy", "numba", "meshio"],
+    extras_require={
+        "optional": ["plotly", "pyopencl"],
+        "lint": ["pydocstyle", "flake8", "pytest", "pytest-xdist"],
+    }
 )
