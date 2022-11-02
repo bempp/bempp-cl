50b1c0fe311c74ff84da09ab69e56a2bdf70f72f
Timo Betcke <timo.betcke@gmail.com>
2022-11-02 13:03:03 +0000

    Added unit test for single element case.

--- test/validation/operators/boundary/test_laplace_boundary.py
+++ test/validation/operators/boundary/test_laplace_boundary.py
@@ -225,3 +225,34 @@ def test_laplace_hypersingular(
     _np.testing.assert_allclose(
         discrete_op.to_dense(), expected, rtol=helpers.default_tolerance(precision)
     )
+
+
+def test_laplace_single_element_p0(
+    default_parameters, helpers, precision, device_interface
+):
+    """Test dense assembler for the Laplace slp with p0 basis on the unit triangle."""
+    import bempp.api
+    from bempp.api import Grid, function_space
+    from bempp.api.operators.boundary.laplace import single_layer
+
+    vertices = _np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]], dtype=_np.float64).T
+    elements = _np.array([[0, 1, 2]]).T
+
+    grid = bempp.api.Grid(vertices, elements)
+
+    space = function_space(grid, "DP", 0)
+
+    value = single_layer(
+        space,
+        space,
+        space,
+        assembler="dense",
+        parameters=default_parameters,
+        device_interface=device_interface,
+        precision=precision,
+    ).weak_form().A[0, 0]
+
+    expected = 0.0798
+
+    assert _np.isclose(value, expected, rtol=1E-3)
+
