60ea162e9bc4211abc88b99b7b716f12a08d7bda
evantwout <7569210+evantwout@users.noreply.github.com>
2022-12-05 11:11:40 -0300

    Unify and vectorise the calculation of the Pade coefficients.

--- bempp/api/operators/boundary/common.py
+++ bempp/api/operators/boundary/common.py
@@ -2,6 +2,7 @@
 from bempp.api.assembly import assembler as _assembler
 from bempp.api.assembly import boundary_operator as _boundary_operator
 from bempp.api.assembly import blocked_operator as _blocked_operator
+import numpy as _np
 
 
 # pylint: disable=too-many-arguments
@@ -99,3 +100,36 @@ def create_multitrace_operator(
     return _blocked_operator.MultitraceOperatorFromAssembler(
         domain, range_, dual_to_range, assembler, descriptor
     )
+
+
+def pade_coeffs(order, angle):
+    """
+    Calculate the coefficients of the Pade series expansion.
+
+    Parameters
+    ----------
+    order : int
+        The order of the expansion.
+    angle : float
+        The branch-cut angle of the expansion.
+
+    Returns
+    -------
+    c_0 : complex
+    a_j : numpy.ndarray[complex]
+    b_j : numpy.ndarray[complex]
+    r_0 : complex
+        The coefficients of the Pade expansion.
+    """
+    idx = _np.arange(order) + 1
+    sin_j = 2 / (2 * order + 1) * _np.sin(idx * _np.pi / (2 * order + 1)) ** 2
+    cos_j = _np.cos(idx * _np.pi / (2 * order + 1)) ** 2
+    z = _np.exp(-1j * angle) - 1
+    z_j = 1 + cos_j * z
+
+    c_0 = _np.exp(0.5j * angle) * (1 + _np.sum(sin_j * z / z_j))
+    a_j = _np.exp(-0.5j * angle) * sin_j / z_j ** 2
+    b_j = _np.exp(-1j * angle) * cos_j / z_j
+    r_0 = c_0 + _np.sum(a_j / b_j)
+
+    return c_0, a_j, b_j, r_0
