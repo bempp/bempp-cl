b7d49ba38760b6a34b9e36ba9364e754edf2d68c
Zhi Li <lizhi44@outlook.com>
2025-08-01 13:41:45 +0200

    Update viewers-after style checks.py

--- bempp_cl/api/external/viewers.py
+++ bempp_cl/api/external/viewers.py
@@ -1,6 +1,8 @@
 """Define interfaces to external viewers."""
 
 import numpy as _np
+import shutil
+import os
 
 
 def visualize(obj, mode=None, transformation=None):
@@ -153,18 +155,12 @@ def visualize_with_gmsh(obj, mode=None, transformation=None):
         )
     outfile.close()
 
-    ## important change for running of windows GUI
-    import shutil
-    import os
-
     gmsh_path = shutil.which("gmsh")
     env = os.environ.copy()
     if gmsh_path.lower().endswith(".bat"):
         subprocess.Popen(f'"{gmsh_path}" "{outfile.name}"', shell=True, env=env)
     else:
         subprocess.Popen([gmsh_path, outfile.name], env=env)
-
-    #subprocess.Popen([GMSH_PATH, outfile.name])
 
 
 def visualize_with_paraview(obj, mode=None, transformation=None):
